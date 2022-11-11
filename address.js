4f8572b8d9d5f14b9c024e4fd8a5f99a2fcb64d6
utdsi <utkarshsinha852@gmail.com>
2022-11-11 23:13:25 +0530

    done

---
+++ address.js
@@ -0,0 +1,16 @@
+let details_submit  = document.getElementById("details-submit")
+
+
+
+
+
+
+details_submit.onclick = ()=>{
+    redirect()
+}
+
+
+function redirect(){
+    window.location.href="./payment.html"
+    //console.log("a")
+}
